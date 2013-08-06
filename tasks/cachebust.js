b4d7f7653ae1637af25ef3233d3afdd89620a9fe
Ben Holland <benholland99@gmail.com>
2013-08-06 03:23:21 -0700

    Initial commit

---
+++ tasks/cachebust.js
@@ -0,0 +1,41 @@
+var fs     = require('fs'),
+    path   = require('path'),
+    crypto = require('crypto');
+
+var regexs = [
+    /<script.+src=['"]([^"']+)["']/gm,
+    /<link rel="stylesheet"[^\>]+href=.*\.css["']/gm
+];
+
+function readFile(file) {
+    var fileData;
+
+    fs.readFile(file, {
+        encoding: 'utf8'
+    }, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        fileData = data;
+
+        regexs.forEach(function(regex) {
+            var matches = data.match(regex) || [];
+            matches.forEach(bustFile);
+        });
+
+        fs.writeFile(file, data, function(err) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(file +' was busted!');
+            }
+        });
+    });
+
+
+    function bustFile(matchedFile) {
+        var hash = crypto.createHash('md5').update(fileData + new Date().getTime(), 'utf8').digest('hex');
+        matchedFile = matchedFile.substring(0, matchedFile.length -1);
+        fileData = fileData.replace(matchedFile, matchedFile+ '?'+ hash);
+    }
+}
