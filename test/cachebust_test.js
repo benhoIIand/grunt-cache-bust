e09fa1a3ef20ab5f9855fafd0efaf50ba5f3e229
Ben Holland <ben.holland@flubit.com>
2014-06-04 16:29:23 +0100

    Added 'delete files' tests

--- test/cachebust_test.js
+++ test/cachebust_test.js
@@ -126,6 +126,16 @@ exports.cachebust = {
         test.done();
     },
 
+    deletesOriginalFiles: function(test) {
+        test.expect(3);
+
+        test.ok(!grunt.file.exists('tmp/assets/replace.css'));
+        test.ok(!grunt.file.exists('tmp/assets/replace.png'));
+        test.ok(!grunt.file.exists('tmp/assets/replace.js'));
+
+        test.done();
+    },
+
     replaceNameAlreadyBusted: function(test) {
         test.expect(6);
 
