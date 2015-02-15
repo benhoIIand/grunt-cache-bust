81678eef8a8ecbc707bf5e97573d83a526a94bb7
Ben Holland <benholland99@gmail.com>
2015-02-15 16:46:50 +0000

    Busted the same file across two tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function(grunt) {
                     }
                 },
                 files: [{
-                    src: ['tmp/replace*.*']
+                    src: ['tmp/replace*.*', 'cdnPath.html']
                 }]
             },
             enableUrlFragmentHint: {
