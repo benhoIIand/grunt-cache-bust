dc6c390f82a13abaa6139c1c4b40f92dfbb8e7c0
Ben Holland <benholland99@gmail.com>
2015-02-15 17:02:27 +0000

    Option to remove frag hints after processing

--- tasks/lib/regexs.js
+++ tasks/lib/regexs.js
@@ -3,5 +3,6 @@
 module.exports = {
     remote: /http:|https:|\/\/|data:image/,
     extension: /(\.[a-zA-Z0-9]{2,4})(|\?.*)$/,
-    urlFragHint: /'(([^']+)#grunt-cache-bust)'|"(([^"]+)#grunt-cache-bust)"/g
+    urlFragHint: /'(([^']+)#grunt-cache-bust)'|"(([^"]+)#grunt-cache-bust)"/g,
+    removeFragHint: /#grunt-cache-bust/g
 };
