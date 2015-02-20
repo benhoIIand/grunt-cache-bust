8450b787159f4aad95762d4e2ee68201e1139787
Ben Holland <benholland99@gmail.com>
2015-02-20 11:04:42 +0000

    Reduced the sizes of some of the test images

--- tests/enableUrlFragments/enableUrlFragments.php
+++ tests/enableUrlFragments/enableUrlFragments.php
@@ -2,7 +2,7 @@
 
 // Example PHP code where assets are just in static strings and must be explicitly identified
 $foo = 'assets/standard.css#grunt-cache-bust';
-$bar = "assets/image2.jpg#grunt-cache-bust";
+$bar = "assets/standard.jpg#grunt-cache-bust";
 
 ?>
 <html>
--- tests/removeUrlFragments/removeUrlFragments_test.js
+++ tests/removeUrlFragments/removeUrlFragments_test.js
@@ -10,7 +10,7 @@ module.exports = {
         var markup = grunt.file.read('tmp/removeUrlFragments/removeUrlFragments.php');
 
         test.ok(markup.match(/'assets\/standard\.[a-z0-9]{16}\.css'/), 'cache bust asset path in single-quoted PHP str');
-        test.ok(markup.match(/"assets\/image2\.[a-z0-9]{16}\.jpg"/), 'cache bust asset path in double-quoted PHP str');
+        test.ok(markup.match(/"assets\/standard\.[a-z0-9]{16}\.jpg"/), 'cache bust asset path in double-quoted PHP str');
         test.ok(markup.match(/"assets\/standard\.[a-z0-9]{16}\.js"/), 'cache bust asset path in double-quoted PHP str');
 
         test.done();
