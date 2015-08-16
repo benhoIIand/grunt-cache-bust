1652595052cf727e78ccd4cb2476ae9f3b5cd5aa
Bertrand Marron <bertrand.marron@ionisx.com>
2015-08-16 18:00:32 +0200

    Add test case for double extensions

--- tests/standard/standard_test.js
+++ tests/standard/standard_test.js
@@ -5,7 +5,7 @@ var grunt = require('grunt');
 module.exports = {
 
     standard: function(test) {
-        test.expect(3);
+        test.expect(4);
 
         var markup = grunt.file.read('tmp/standard/standard.html');
 
@@ -13,17 +13,21 @@ module.exports = {
         test.ok(markup.match(/assets\/standard\.[a-z0-9]{16}\.jpg/), 'testing assets/standard.jpg');
         test.ok(markup.match(/assets\/standard\.[a-z0-9]{16}\.js/ ), 'testing assets/standard.js');
 
+        test.ok(markup.match(/assets\/extensions\.css\.[a-z0-9]{16}\.css/), 'testing assets/extensions.css.css');
+
         test.done();
     },
 
     relative: function(test) {
-        test.expect(3);
+        test.expect(4);
 
         var markup = grunt.file.read('tmp/standard/subfolder/relative-standard.html');
 
         test.ok(markup.match(/assets\/standard\.[a-z0-9]{16}\.css/), 'testing relative file path of assets/standard.css');
         test.ok(markup.match(/assets\/standard\.[a-z0-9]{16}\.jpg/), 'testing relative file path of assets/standard.jpg');
         test.ok(markup.match(/assets\/standard\.[a-z0-9]{16}\.js/ ), 'testing relative file path of assets/standard.js');
+
+        test.ok(markup.match(/assets\/extensions\.css\.[a-z0-9]{16}\.css/), 'testing relative file path of assets/extensions.css.css');
 
         test.done();
     }
