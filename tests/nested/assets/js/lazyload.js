b814d225f74a3d5c9c58d24fb80f77ce6303d282
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-04-21 10:53:32 -0300

    removing comments

--- tests/nested/assets/js/lazyload.js
+++ tests/nested/assets/js/lazyload.js
@@ -2,8 +2,7 @@ angular.module('main')
 .config(['$ocLazyLoadProvider', function ($ocLazyLoadProvider) {
 	$ocLazyLoadProvider.config({
 		debug: false,
-		modules: [ /* Definição dos alias dos módulos da aplicação */
-			/* lib */
+		modules: [
 			{ name: 'comp', files: [ 'assets/js/comp.js' ] }
         ]
     });
